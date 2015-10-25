2d18e29c25d2e45389a3c00276e15712aa41f050
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-10-25 14:39:33 -0400

    Homework3

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -10,16 +10,20 @@
                     templateUrl: "./home/home.view.html"
                 })
                 .when("/register", {
-                    templateUrl: "./register/register.view.html"
+                    templateUrl: "./register/register.view.html",
+                    controller: "RegisterController"
                 })
                 .when("/login", {
-                    templateUrl: "./login/login.view.html"
+                    templateUrl: "./login/login.view.html",
+                    controller: "LoginController"
                 })
                 .when("/profile", {
-                    templateUrl: "./profile/profile.view.html"
+                    templateUrl: "./profile/profile.view.html",
+                    controller: "ProfileController"
                 })
                 .when("/form", {
-                    templateUrl: "./form/form.view.html"
+                    templateUrl: "./form/form.view.html",
+                    controller: "FormController"
                 })
                 .otherwise({
                     redirectTo: "/home"
