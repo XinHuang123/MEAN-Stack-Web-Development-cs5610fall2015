8a6e04395243e92453a8247d6ba875e9b8a15da1
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-11-16 00:59:23 -0500

    server side

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//require("./experiments/nodejs/expressjs/server.js")(app);
+//require("./public/angularjsClient/server.js")(app);
+//require("./public/angularjsClient/jsonp/server/MovieService.js")(app);
+//require("./public/movies/server/app.js")(app);
+require("./public/assignment/server/app.js")(app);
 
 
- var express = require('express');
- var app = express();
 
- app.use(express.static(__dirname + '/public'));//host the static content in public directory
-
- //bodyparser for update and create
- var bodyParser = require('body-parser');
- var multer = require('multer'); // v1.0.5
- app.use(bodyParser.json()); // for parsing application/json
- app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
- app.use(multer());//for parsing multipart/form-data
-
- //require("./public/assignment/server/services/user.service.js")(app);
- //require("./public/assignment/server/services/form.service.js")(app);
- //require("./public/assignment/server/services/field.service.js")(app);
- require("./public/assignment/server/app.js")(app);
-
- var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
- var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
- app.listen(port,ipaddress);
-
+app.listen(port, ipaddress);
 
 
