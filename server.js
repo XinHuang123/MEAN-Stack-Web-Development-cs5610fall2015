99db1caff016366dcd2e24ff7d783f5b17ea6802
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-11-16 00:16:50 -0500

    server side

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var multer = require('multer');
 
-var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port 		= process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'));//host the static content in public directory
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(multer());//for parsing multipart/form-data
-
-require("./public/assignment/server/services/user.service.js")(app);
-require("./public/assignment/server/services/form.service.js")(app);
-require("./public/assignment/server/services/field.service.js")(app);
-
-app.listen(port,ipaddress);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//require("./public/project/server/app.js")(app);
+//require("./public/assignment/server/app.js")(app);
+app.listen(port, ipaddress);
