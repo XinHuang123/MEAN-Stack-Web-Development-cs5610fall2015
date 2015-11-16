060529a91b339c83d0f38dd3ffdbb04396bd4924
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-11-16 00:56:57 -0500

    server side

--- server.js
+++ server.js
@@ -12,16 +12,15 @@
  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
  app.use(multer());//for parsing multipart/form-data
 
- require("./public/assignment/server/services/user.service.js")(app);
- require("./public/assignment/server/services/form.service.js")(app);
- require("./public/assignment/server/services/field.service.js")(app);
+ //require("./public/assignment/server/services/user.service.js")(app);
+ //require("./public/assignment/server/services/form.service.js")(app);
+ //require("./public/assignment/server/services/field.service.js")(app);
+ require("./public/assignment/server/app.js")(app);
 
  var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
  app.listen(port,ipaddress);
-
-
 
 
 
