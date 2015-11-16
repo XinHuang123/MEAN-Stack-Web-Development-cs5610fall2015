c20a387c7ed83e5ad4770b321ed88ecd6c15fb72
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-11-16 00:46:15 -0500

    test

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-//require("./public/project/server/app.js")(app);
-require("./public/assignment/server/app.js")(app);
 app.listen(port, ipaddress);
 
 
