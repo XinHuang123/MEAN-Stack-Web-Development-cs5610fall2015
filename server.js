5dd291696689436c6b450381b9929f35ac8be896
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-12-01 12:04:14 -0500

    Hw5

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.use(multer());//for parsing multipart/form-data
 
 var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
 
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
 var mongoose = require('mongoose');
 mongoose.connect(connectionString);//connect to database
 var db = mongoose.connection;
