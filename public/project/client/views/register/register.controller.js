f71bfc988d47d83129cf56cd966f641e19913429
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-12-07 09:17:48 -0500

    refactor register

--- public/project/client/views/register/register.controller.js
+++ public/project/client/views/register/register.controller.js
@@ -5,62 +5,44 @@
         .module("Restaurant")
         .controller("RegisterController", RegisterController);
 
-    function RegisterController($scope, $location, $rootScope, UserService) {
-        $scope.$location = $location;
-        $scope.register = register;
+    function RegisterController(UserService, $location, $rootScope) {
+        var model = this;
+        model.getAllUsers = getAllUsers;
+        model.register = register;
 
-        function register() {
-            $scope.regError = null;
+        function init() {
+            UserService.FindAll()
+                .then(function(users){
+                    model.allUsers = users;
+                });
+        }
+        init();
 
-            if ($scope.userName && $scope.password && $scope.verifyPassword && $scope.email) {
-                if ($scope.password != $scope.verifyPassword) {
-                    $scope.regError = "Passwords do not match";
-                } else {
-                    UserService.FindAll()
-                        .then(function (users) {
+        function getAllUsers() {
+            UserService.FindAll()
+                .then(function(users){
+                    model.allUsers = users;
+                });
+        }
 
-                            for (var i = 0; i < users.length; i++) {
-                                if (users[i].username === $scope.userName) {
-                                    var message = "User Name already exists. Please choose a different one. We accept only letters and numbers in usernames";
-
-                                    if ($scope.regError) {
-                                        $scope.regError += " | " + message;
-                                    } else {
-                                        $scope.regError = message;
-                                    }
-                                }
-                                if (users[i].email === $scope.email) {
-                                    var message = "Email already in use";
-
-                                    if ($scope.regError) {
-                                        $scope.regError += " | " + message;
-                                    } else {
-                                        $scope.regError = message;
-                                    }
-                                }
-                            }
-
-                            if (!$scope.regError) {
-                                var user = {
-                                    firstname: $scope.firstName,
-                                    lastname: $scope.lastName,
-                                    username: $scope.userName,
-                                    password: $scope.password,
-                                    email: $scope.email
-                                };
-
-                                UserService.Create(user)
-                                    .then(function (newUser) {
-                                        $rootScope.currentUser = newUser;
-                                        $rootScope.$broadcast('authenticate', newUser);
-                                        $location.url('/home');
-                                    });
-                            }
-                        });
-                }
-            } else {
-                $scope.regError = "All fields are required"
-            }
+        function register(){
+            var username = model.user.userName;
+            var pwd = model.user.password;
+            var email = model.user.email;
+            var firstname = model.user.firstName;
+            var lastname = model.user.lastName;
+            var userObj = {username: username, password: pwd , email: email,firstname:firstname,lastname:lastname};
+            var user = UserService.Create(userObj)
+                .then(function(user){
+                    if(user != null) {
+                        //$rootScope.curusername = user.username;
+                        //$rootScope.curpwd = user.password;
+                        //$rootScope.curid = user._id;
+                        //$rootScope.curemail = user.email;
+                        $rootScope.currentUser=user;
+                        $location.url("/profile");
+                    }
+                });
         }
     }
 })();
