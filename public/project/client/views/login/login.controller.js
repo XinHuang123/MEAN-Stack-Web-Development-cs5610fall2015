9ba392f234ba0d52acfe3d1d985602147cb2594b
XinHuang123 <Huang.xin1@husky.neu.edu>
2015-12-07 09:51:49 -0500

    refactor login,register,profile

--- public/project/client/views/login/login.controller.js
+++ public/project/client/views/login/login.controller.js
@@ -1,5 +1,4 @@
 "use strict";
-
 (function() {
     angular
         .module("Restaurant")
@@ -28,9 +27,41 @@
                         $location.url("/home");
                     } else {
                         $scope.loginerror = true;
-                        $scope.loginErrorMessage = "Your credentials did not match our records. Please try again!"
+                        $scope.loginErrorMessage = "Oh Snap! Your credentials did not match our records. Please try again!"
                     }
                 });
         }
     }
 })();
+//"use strict";
+//(function() {
+//    angular
+//        .module("Restaurant")
+//        .controller("LoginController", LoginController);
+//    function LoginController(UserService, $rootScope, $location) {
+//        var model = this;
+//        model.login = login;
+//
+//        function login() {
+//            var username = model.userName;
+//            var pwd = model.password;
+//            UserService.FindByAuth(username, pwd)
+//                .then(function(user){
+//                    if (user != null) {
+//                        //$rootScope.curusername = user[0].username;
+//                        //$rootScope.curpwd = user[0].password;
+//                        //console.log(user[0]._id + " " + user[0].id);
+//                        //$rootScope.curid = user[0]._id;
+//                        //$rootScope.curemail = user[0].email;
+//                        //$rootScope.firstname = user[0].firstName;
+//                        //$rootScope.lastname = user[0].lastName;
+//                        $rootScope.currentUser=user;
+//                        $location.url("/home");
+//                    }
+//                }
+//            )};
+//
+//    }
+//})();
+
+
--- public/project/client/views/profile/profile.controller.js
+++ public/project/client/views/profile/profile.controller.js
@@ -1,44 +1,53 @@
 "use strict";
-
 (function() {
     angular
         .module("Restaurant")
         .controller("ProfileController", ProfileController);
 
-    function ProfileController(UserService,$rootScope) {
-        var model = this;
-        model.update = update;
-        var loginuser=$rootScope.currentUser;
-
-        //model.username = $rootScope.curusername;
-        //model.password = $rootScope.curpwd;
-        //model.email = $rootScope.curemail;
-        //model.firstname = $rootScope.firstname;
-        //model.lastname = $rootScope.lastname;
-        model.username = loginuser.username;
-        model.password = loginuser.password;
-        model.email = loginuser.email;
-        model.firstname = loginuser.firstname;
-        model.lastname = loginuser.lastname;
-        function update(){
-            var userobj = {username: model.username, password: model.password, id: loginuser._id,
-                email: model.email, firstname: model.firstname, lastname: model.lastname};
-
-            UserService.Update(loginuser._id, userobj)
-                .then(function(user){
-                    if(user != null) {
-                        console.log("user : " + user);
-                        console.log("Updated username: " + user.username);
-                        $rootScope.currentUser=userobj;
-                        //$rootScope.curusername = user.username;
-                        //$rootScope.curpwd = user.password;
-                        //$rootScope.curid = user._id;
-                        //$rootScope.curemail = user.email;
-                        //$rootScope.firstname = user.firstName;
-                        //$rootScope.lastname = user.lastName;
-                        //$rootScope.currentUser=
-                    }
-                });
-        }
+    function ProfileController($scope, $location) {
+        $scope.$location = $location;
     }
 })();
+//"use strict";
+//(function() {
+//    angular
+//        .module("Restaurant")
+//        .controller("ProfileController", ProfileController);
+//
+//    function ProfileController(UserService,$rootScope) {
+//        var model = this;
+//        model.update = update;
+//        var loginuser=$rootScope.currentUser;
+//
+//        //model.username = $rootScope.curusername;
+//        //model.password = $rootScope.curpwd;
+//        //model.email = $rootScope.curemail;
+//        //model.firstname = $rootScope.firstname;
+//        //model.lastname = $rootScope.lastname;
+//        model.username = loginuser.username;
+//        model.password = loginuser.password;
+//        model.email = loginuser.email;
+//        model.firstname = loginuser.firstname;
+//        model.lastname = loginuser.lastname;
+//        function update(){
+//            var userobj = {username: model.username, password: model.password, id: loginuser._id,
+//                email: model.email, firstname: model.firstname, lastname: model.lastname};
+//
+//            UserService.Update(loginuser._id, userobj)
+//                .then(function(user){
+//                    if(user != null) {
+//                        console.log("user : " + user);
+//                        console.log("Updated username: " + user.username);
+//                        $rootScope.currentUser=userobj;
+//                        //$rootScope.curusername = user.username;
+//                        //$rootScope.curpwd = user.password;
+//                        //$rootScope.curid = user._id;
+//                        //$rootScope.curemail = user.email;
+//                        //$rootScope.firstname = user.firstName;
+//                        //$rootScope.lastname = user.lastName;
+//                        //$rootScope.currentUser=
+//                    }
+//                });
+//        }
+//    }
+//})();
